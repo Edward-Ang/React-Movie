74e6fc766b4f8c9f49689510de6d30c61b938632
Edward Ang <wei.hen.ang@gmail.com>
2024-05-31 01:34:15 +0800

    add css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ import './MovieCard.css';
 const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
-      <Link to={`/movie/${movie.id}`}>
+      <Link to={`/movie/${movie.id}`} className='movie-link'>
         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
         <h3 className='movie-title'>{movie.title}</h3>
       </Link>
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -22,6 +22,7 @@ const MovieDetails = () => {
   return (
     <div>
       <h1>{movie.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
       <p>{movie.overview}</p>
       <p>Release Date: {movie.release_date}</p>
       <p>Rating: {movie.vote_average}</p>
