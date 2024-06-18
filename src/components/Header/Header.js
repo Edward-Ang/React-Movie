521fd6f05c828dfc779b4e258889a1f3420868f3
Edward Ang <wei.hen.ang@gmail.com>
2024-06-18 16:42:54 +0800

    + mobile view (Home)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 // src/components/Header.js
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { AiOutlineHeart, AiOutlineUser, AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineUser, AiOutlineBell, AiOutlineSearch, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useMediaQuery } from 'react-responsive';
 import './Header.css';
+import './HeaderMedia.css'
 
 const Header = ({ userDetails, toggleProfileVisible, toggleLoginVisible }) => {
   const user = userDetails.user;
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
   const [isScrolled, setIsScrolled] = useState(true);
+  const [visibleDropdown, setVisibleDropdown] = useState(null);
+  const mobileWidth = useMediaQuery({ maxWidth: 480 });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,6 +30,10 @@ const Header = ({ userDetails, toggleProfileVisible, toggleLoginVisible }) => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const handleDropdownClick = () => {
+    setVisibleDropdown(!visibleDropdown);
+  };
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -68,21 +76,66 @@ const Header = ({ userDetails, toggleProfileVisible, toggleLoginVisible }) => {
           <button type="submit" className="search-button"><AiOutlineSearch className='search-icon' /></button>
         </div>
       </form>
-      <div className='utility'>
-        <button className='utility-btn' onClick={handleFav} ><AiOutlineHeart className='utility-icon' /></button>
-        <button className='utility-btn'><AiOutlineBell className='utility-icon' /></button>
-        {user ? (
-          <img
-            className='profile-pic'
-            src={user.picture || '/images/fox-avatar.png'}
-            alt={user.name}
-            title={user.name}
-            onClick={handleProfile}
-          />) : (
-          <button className='utility-btn' onClick={handleLogin}><AiOutlineUser className='utility-icon' /></button>
-        )}
-      </div>
-    </header>
+      {mobileWidth ? (
+        <>
+          <button className='utility-btn' onClick={handleDropdownClick}>
+            {visibleDropdown ? (<AiOutlineClose className='utility-icon' />) : (<AiOutlineMenu className='utility-icon' />)}
+          </button>
+          {visibleDropdown && (
+            <div className="dropdown-menu" id='header-dropdown-menu'>
+              <div clasName="dropdown-item" id='header-dropdown-item'>
+                {user ? (
+                  <img
+                    className='profile-pic'
+                    src={user.picture || '/images/fox-avatar.png'}
+                    alt={user.name}
+                    title={user.name}
+                    onClick={handleProfile}
+                  />) : (
+                  <button className='utility-btn' id='profile-btn' onClick={handleLogin}>
+                    <div className='btn-container'>
+                      <span>Profile</span>
+                      <AiOutlineUser className='utility-icon' id='user-icon' />
+                    </div>
+                  </button>
+                )}
+              </div>
+              <div clasName="dropdown-item" id='header-dropdown-item'>
+                <button className='utility-btn' id='favourite-btn' onClick={handleFav} >
+                  <div className='btn-container'>
+                    <span>Favourite</span>
+                    <AiOutlineHeart className='utility-icon' id='heart-icon' />
+                  </div>
+                </button>
+              </div>
+              <div clasName="dropdown-item" id='header-dropdown-item'>
+                <button className='utility-btn' id='notification-btn' >
+                  <div className='btn-container'>
+                    <span>Notification</span>
+                    <AiOutlineBell className='utility-icon' id='bell-icon' />
+                  </div>
+                </button>
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className='utility'>
+          <button className='utility-btn' onClick={handleFav} ><AiOutlineHeart className='utility-icon' /></button>
+          <button className='utility-btn'><AiOutlineBell className='utility-icon' /></button>
+          {user ? (
+            <img
+              className='profile-pic'
+              src={user.picture || '/images/fox-avatar.png'}
+              alt={user.name}
+              title={user.name}
+              onClick={handleProfile}
+            />) : (
+            <button className='utility-btn' onClick={handleLogin}><AiOutlineUser className='utility-icon' /></button>
+          )}
+        </div>
+      )}
+    </header >
   );
 };
 
